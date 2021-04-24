442af4d44b5897c50d51e5029b9676e542c02ab7
Guillaume Grossetie <ggrossetie@gmail.com>
2021-04-24 20:04:03 +0200

    Fix tests

--- test/chevrotain-lexer-test.js
+++ test/chevrotain-lexer-test.js
@@ -1,23 +1,37 @@
 'use strict'
 
 import { Lexer } from 'chevrotain'
+import assert from 'assert'
 import { ExampleBlockDelimiter, SidebarBlockDelimiter, WhiteSpace, Text } from '../lib/engines/chevrotain/lexer.js'
 
 describe('AsciiDocChevrotainLexer', () => {
-  it('should lex', async () => {
+  it('should tokenize unterminated example block', async () => {
     const lexer = new Lexer([
       WhiteSpace,
       ExampleBlockDelimiter,
       SidebarBlockDelimiter,
       Text
     ])
-    let result = lexer.tokenize(`Here's 4 equals signs: ====
+    const result = lexer.tokenize(`Here's 4 equals signs: ====
  ====`)
-    console.log(result)
-    result = lexer.tokenize(`====
+    assert.equal(result.tokens.length, 2)
+    assert.equal(result.tokens[0].image, `Here's 4 equals signs: ====`)
+    assert.equal(result.tokens[1].image, `====`)
+  })
+  it('should tokenize example block', async () => {
+    const lexer = new Lexer([
+      WhiteSpace,
+      ExampleBlockDelimiter,
+      SidebarBlockDelimiter,
+      Text
+    ])
+    const result = lexer.tokenize(`====
 This is an example.
 ====`)
-    console.log(result)
+    assert.equal(result.tokens.length, 3)
+    assert.equal(result.tokens[0].image, `====`)
+    assert.equal(result.tokens[1].image, `This is an example.`)
+    assert.equal(result.tokens[2].image, `====`)
   })
 })
 
