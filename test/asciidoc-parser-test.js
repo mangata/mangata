29550f9177c1e059652183ab7c7abfcc6e3c1364
Guillaume Grossetie <ggrossetie@gmail.com>
2021-11-01 13:49:13 +0100

    Fix tests

--- test/asciidoc-parser-test.js
+++ test/asciidoc-parser-test.js
@@ -1,18 +1,17 @@
 'use strict'
 
 import assert from 'assert'
-import chai from 'chai'
 import { promises as fs } from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import AsciiDocParser from '../lib/index.js'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 import AsciidoctorEngine from '../lib/engines/asciidoctor/index.js'
 import TextlintEngine from '../lib/engines/textlint/index.js'
 import ChevrotainEngine from '../lib/engines/chevrotain/index.js'
 
 import util from 'util'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 util.inspect.defaultOptions.depth = null
 
 const engines = [
@@ -35,25 +34,41 @@ for (const engine of engines) {
       context('DocumentNode', () => {
         it('should create sparse DocumentNode from empty document', async () => {
           const result = parseFixture(await loadFixture('empty.adoc'))
-          assert.equal(result.type, 'Document')
-          assert.equal(result.raw, '')
-          assert.deepEqual(result.children, [])
+          assert.deepEqual(result, {
+            body: []
+          })
         })
         it('should create DelimitedBlock for each sibling delimited block of different type', async () => {
           const result = parseFixture(await loadFixture('different-blocks.adoc'))
-          assert.equal(result.children.length, 2)
-          result.children.forEach((block) => {
-            assert.equal(block.type, 'DelimitedBlock')
-            assert.equal(block.children.length, 1)
-            assert.equal(block.children[0].type, 'Paragraph')
+          assert.deepEqual(result, {
+            body: [
+              {
+                type: 'Example',
+                children: [
+                  {
+                    type: 'Paragraph',
+                    value: 'Let me show you the way.'
+                  }
+                ]
+              },
+              {
+                type: 'Sidebar',
+                children: [
+                  {
+                    type: 'Paragraph',
+                    value: 'Let\'s go on a little trip.'
+                  }
+                ]
+              }
+            ]
           })
         })
         it('should create listing blocks', async () => {
-          const result = parseFixture(await loadFixture('listing-block.adoc'))
+          //const result = parseFixture(await loadFixture('listing-block.adoc'))
           // fixme: write assertions
         })
         it('should create nested blocks', async () => {
-          const result = parseFixture(await loadFixture('nested-blocks.adoc'))
+          //const result = parseFixture(await loadFixture('nested-blocks.adoc'))
           // fixme: write assertions
         })
       })
